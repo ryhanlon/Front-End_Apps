4a55b9e0258e7c7b9c1423f7f55d902878053c80
ryhanlon <ryhanlon@gmail.com>
2018-08-11 23:35:36 -0700

    feat: Add event handler to select feature kitty

--- cat_clicker/catClicker.js
+++ cat_clicker/catClicker.js
@@ -3,31 +3,31 @@ const cats = [
 	{
 		name: 'Meme',
 		clicks: 0,
-		image: 'static/images/kitty1.jpg',
+		image: 'static/images/Meme.jpg',
 		copyright: 'photo by Sonoraxus'
 	},
 	{
 		name: 'Snooks',
 		clicks: 0,
-		image: 'static/images/kitty2.jpg',
+		image: 'static/images/Snooks.jpg',
 		copyright: 'photo by Paul Reynolds'
 	},
 	{
 		name: 'Strings',
 		clicks: 0,
-		image: 'static/images/kitty3.jpg',
+		image: 'static/images/Strings.jpg',
 		copyright: 'photo by HolgersFotografie'
 	},
 	{
 		name: 'Arbolito',
 		clicks: 0,
-		image: 'static/images/kitty4.jpg',
+		image: 'static/images/Arbolito.jpg',
 		copyright: 'photo by Kessa'
 	},
 	{
-		name: 'Cansada',
+		name: 'Lambda',
 		clicks: 0,
-		image: 'static/images/kitty5.jpg',
+		image: 'static/images/Lambda.jpg',
 		copyright: 'photo by Max Pixel'
 	},
 
@@ -38,13 +38,15 @@ const cats = [
 let counter = 0;
 
 
-const kittyCount = () => {
-	const countContainer = document.querySelector('.num-clicks');
-	counter += 1;
-
-	countContainer.innerHTML = `Number of Clicks: ${counter}`;
-	console.log('clicking the kittie');
-};
+// const kittyCount = () => {
+// 	const countContainer = document.querySelector('.cat-container img-holder img');
+// 	let kittyPets = cats.clicks;
+//
+// 	 kittyPets += 1;
+//
+// 	countContainer.innerHTML = `Number of Clicks: ${counter}`;
+// 	console.log('clicking the kittie' + kittyPets);
+// };
 
 
 // Click the kittie
@@ -59,16 +61,43 @@ const kittyCount = () => {
 // };
 
 
+const kittyChoice = (event) => {
+	// add picture to feature space to pet
+	let pictureName = event.target.id;
+	console.log(pictureName);
+	kittyFeature(pictureName);
+};
+
 // eventListener for clicks
 const eventListener = () => {
-	const kittyClicks = document.querySelector('.img-holder');
+	// const kittyClicks = document.querySelector('.thumbnail-container cat-container button');
 
-	kittyClicks.addEventListener('click', kittyCount);
+	$('button').on('click', kittyChoice);
 
 };
 
-const kittyFeature = () => {
+
+// const eventListener2 = () => {
+// 	const kittyClicks = document.querySelector('.img-holder');
+//
+// 	kittyClicks.addEventListener('click', kittyCount);
+//
+// };
+
+const kittyFeature = (pictureName) => {
 	let catFeature = document.querySelector('.main-container');
+	$(catFeature).empty();
+
+		let addFeatureCat = `<section class="cat-container">
+			<h3 class="kitty-name thum-design">${pictureName}</h3>
+			 <p class="num-clicks thum-design ${pictureName}">pets</p>
+			 <div class="img-holder">
+		 	<img id=${pictureName} class="picture" src='static/images/${pictureName}.jpg' alt="Cute kitten" />
+		 	</div>
+		 	</section>`;
+
+		catFeature.innerHTML += addFeatureCat;
+	// eventListener2();
 
 };
 
@@ -76,16 +105,14 @@ const kittyFeature = () => {
 const kittyThumbnails = (array) => {
 let catGallery = document.querySelector('.thumbnail-container');
 
-	cats.forEach(cat => {
+	cats.forEach((cat, index) => {
 		let addCat = `<section class="cat-container">
 		 <h3 class="kitty-name thum-design">${cat.name}</h3>
-		 <p class="num-clicks thum-design ${cat.name}">Click to pet Me!</p>
+		 <button id=${cat.name} class="thum-design ${cat.name}">Click to pet me!</button>
 		 <div class="img-holder">
-		 <img id=${cat.name} class="picture" src=${cat.image} alt="Cute kitten">
+		 <img id=${index} class="picture" src=${cat.image} alt="Cute kitten">
 		 </div>
 		 </section>`;
-
-		console.log(cat.image);
 
 		catGallery.innerHTML += addCat;
 	});
